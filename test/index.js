caed48ea303ac00914f185f4d9906513af8c849b
SIMDD <dev.simdd@gmail.com>
2017-08-11 18:09:49 +0800

    add req file and res file

--- test/index.js
+++ test/index.js
@@ -4,7 +4,6 @@
 
 'use strict'
 
-const fs = require('fs')
 const path = require('path')
 
 let Son = require('../index')
@@ -22,14 +21,7 @@ app.use(function (req, res, next) {
 })
 
 app.use(function (req, res, next) {
-    res.writeHead(200, {'content-type': 'text/html'})
-    let stream = fs.createReadStream(path.resolve(__dirname, './index.html'), {flags: 'r', encoding: null})
-    stream.on('error', function () {
-        res.writeHead(500, {'content-type': 'text/html'})
-        res.end('Server Error')
-    })
-
-    stream.pipe(res)
+    res.html(path.resolve(__dirname, './index.html'))
 })
 
 app.listen(80)
